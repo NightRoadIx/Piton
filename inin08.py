f7041e92bcaa0969acaf73c9fde422f6d6c32922
NightRoadIx <quintanarxbio@gmail.com>
2020-02-07 17:34:58 -0600

    Add files via upload

---
+++ inin08.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 17:21:19 2020
+
+@author: s_bio
+"""
+
+'''
+    MANEJO DE EXCEPCIONES
+    El manejo de exepciones permite controlar los errores de un programa en tiempo de ejecución .
+    Generalmente, cuando un error ocurre (división entre 0, operar variables de tipos diferentes)
+    el programa normalmente se detiene y genera un mensaje de error.
+    El término técnico para esto es que el programa lanza una excepción (o arroja un error).
+    		
+    Para esto se utiliza la instrucción try-except-else-finally
+'''
+
+try:
+    # La variable x no esta definida, entonces esto produce un error en tiempo de ejecución
+    print(x)
+except:
+    # Se muestra cuando ocurre la excepción
+    print("Ha ocurrido una excepción")
+else:
+    # No ocurre la excepción
+    print("No exisitió ninguna excepción")
+finally:
+    # Esto se ejecuta siempre, exista o no una excepción
+    print("El bloque try - except termino")
+
+
+# Puede ocurrir una excepción cuando se realiz auna división entre 0
+def divide(x, y):
+    try:
+        # Se realiza la división
+        resultado = x // y
+    except:
+        print("Error, no se puede dividir entre 0")
+    else:
+        print("Resultado es : " + str(resultado))
+
+divide(5,0)
