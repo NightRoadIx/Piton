4d4b8a255178b1f3e630b2334df32292a5ade7f2
NightRoadIx <quintanarxbio@gmail.com>
2020-10-17 18:12:20 -0500

    Update inin08.py

--- inin08.py
+++ inin08.py
@@ -8,6 +8,12 @@
     Para esto se utiliza la instrucción try-except-else-finally
 '''
 
+#%%
+a = 5 / 0
+
+#%%
+
+#x = 5
 try:
     # La variable x no esta definida, entonces esto produce un error en tiempo de ejecución
     print(x)
@@ -21,15 +27,46 @@ finally:
     # Esto se ejecuta siempre, exista o no una excepción
     print("El bloque try - except termino")
 
+#%%
 
-# Puede ocurrir una excepción cuando se realiz auna división entre 0
+# Puede ocurrir una excepción cuando se realiza una división entre 0
 def divide(x, y):
     try:
         # Se realiza la división
         resultado = x // y
-    except:
+    except ZeroDivisionError:
         print("Error, no se puede dividir entre 0")
     else:
         print("Resultado es : " + str(resultado))
 
 divide(5,0)
+print("Sin embargo, el programa sigue")
+
+#%%
+# También es posible que se genere un error al intentar ingresar
+# a un elemento inexistente en una lista
+a = [1, 2, 3]
+try:
+    print("El segundo elemento {:}".format(a[1]))
+    # Esto generará un error
+    print("El cuarto elemento {:}".format(a[3]))
+except IndexError:
+    print("Se intento ingresar a un índice no válido")
+finally:
+    print("Siempre se ejecuta")
+
+#%%
+
+# En ocasiones también es necesario el manejo de dos o más
+# excepciones
+try:
+    a = 3
+    if a < 4:
+        # Lanzar una excepción ZeroDivisionError
+        b = a / (a - 3)
+    # Esto lanza otra excepción NameError
+    print("Valor de b: ", b)
+except (ZeroDivisionError, NameError):
+    print("Un error sucedio y fue controlado")
+
+print("Sin embargo, el programa sigue")
