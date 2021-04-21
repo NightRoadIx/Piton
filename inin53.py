067c1f8403556cf91601d48bf86b2cb13842244e
NightRoadIx <quintanarxbio@gmail.com>
2021-04-21 02:59:38 -0500

    Add files via upload

--- inin53.py
+++ inin53.py
@@ -4,63 +4,48 @@ Created on Tue Apr 20 17:09:32 2021
 
 @author: s_bio
 """
+#%%
+from PIL import Image, ImageDraw
+# Python Image Library
+
+# Con esto se genera una lista con las imágenes que conformarán el gif
+# animado
+images = []
 
 #%%
-# Recursividad
-# La recursividad es cuando una función se llama a si misma. 
-# Típicamente, una función recursiva posee una condición de terminación y una
-# o varias llamadas recursivas a si misma
-
-# Por ejemplo, para calcular la elevación de un número a un exponente, 
-# matemáticamente se puede definir 
-def exponente(x, n):
-    if n == 0:
-        return 1
-    else:
-        return x * exponente(x, n-1)
-
-#%%
-# Esto puede realizarse más rápidamente, si se realizan sucesivamente
-# elevar al cuadrado
-def exponencialRapida(x, n):
-    if  n == 0:
-        return 1
-    elif n % 2 == 0:
-        return exponencialRapida(x*x, n/2)
-    else:
-        return x * exponencialRapida(x, n-1)
-
-
-print(exponencialRapida(4, 5))
-
-
+# Colocar los valores de la imagen
+# Ancho
+width = 200
+# Centro
+center = width // 2
+# Colores (se usa RGB)
+color_1 = (0, 0, 0)
+color_2 = (255, 255, 255)
+# Radio máximo 
+max_radius = int(center * 1.5)
+# Pasos con los que se generará la animación
+step = 8
 
 #%%
-# Función recursiva para el cálculo de la serie de Fibonacci
-def fib(n):
-    if n is 0 or n is 1:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
-
-# Muestra el valor de la serie
-fib(8)
+# Ir generando el primer círculo de color negro
+for i in range(0, max_radius, step):
+    # Con esto se genera una imagen nueva
+    im = Image.new('RGB', (width, width), color_1)
+    # Dibujar la imagen generada
+    draw = ImageDraw.Draw(im)
+    # Con esto se dibuja una elipse, de acuerdo a la perspectiva, se tiene que
+    # crear una elipse en lugar de un círculo para que no se vea tan feo
+    draw.ellipse((center - i, center - i, center + i, center + i), fill=color_2)
+    # Ir añadiendo los cuadros generados a la lista
+    images.append(im)
 
 #%%
+for i in range(0, max_radius, step):
+    im = Image.new('RGB', (width, width), color_2)
+    draw = ImageDraw.Draw(im)
+    draw.ellipse((center - i, center - i, center + i, center + i), fill=color_1)
+    images.append(im)
 
-# Con la siguiente función se traza todas las llamadas a la función fib
-# se escribe una función de orden superior que regresa una nueva función
-# la cual imrpime cuando la función fib es llamada
-def trazar(f):
-    f.indent = 0
-    def g(x):
-        print('|  ' * f.indent + '|--', f.__name__, x)
-        f.indent += 1
-        valor = f(x)
-        print('|  ' * f.indent + '|--', 'Regresar ', repr(valor))
-        f.indent -= 1
-        return valor
-    return g
-
-fib = trazar(fib)
-print(fib(4))
+#%%
+images[0].save('pillow_imagedraw.gif',
+               save_all=True, append_images=images[1:], optimize=False, duration=40, loop=0)
